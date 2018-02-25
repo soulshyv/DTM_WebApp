3a9172813fab94dde7980937ba0b3669979786f2
soulshyv <fra62@sfr.fr>
2018-02-26 00:42:31 +0100

    Fonction générale d'update avec switch des différents updates possibles, reste à faire les formulaires adéquats

--- Main/Controllers/CharactersController.cs
+++ Main/Controllers/CharactersController.cs
@@ -57,7 +57,7 @@ namespace Main.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> UpdateCharac(CharacterDetailsViewModel details)
+        public async Task<IActionResult> Update(CharacterDetailsViewModel details)
         {
             if (!ModelState.IsValid)
             {
@@ -69,196 +69,72 @@ namespace Main.Controllers
                 return NotFound();
             }
 
-            if (details.Charac.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
+            var nomPerso = details.Charac.Nom;
+            if (string.IsNullOrWhiteSpace(nomPerso))
             {
                 return NotFound();
             }
 
-            await DtmRepositoryUpdate.UpdatePerso(details.Charac);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateCaracs(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
+            if (!details.Caracs.IsAnyNullOrEmpty())
             {
-                return RedirectToAction("Index");
+                await DtmRepositoryUpdate.UpdateCaracsPerso(details.Caracs, nomPerso);
             }
-
-            if (details == null)
+            else
             {
-                return NotFound();
+                if (!details.Charac.IsAnyNullOrEmpty())
+                {
+                    await DtmRepositoryUpdate.UpdatePerso(details.Charac);
+                }
+                else
+                {
+                    if (!details.Demons.IsAnyNullOrEmpty())
+                    {
+                        await DtmRepositoryUpdate.UpdateDemonsPerso(details.Demons, nomPerso);
+                    }
+                    else
+                    {
+                        if (!details.Dons.IsAnyNullOrEmpty())
+                        {
+                            await DtmRepositoryUpdate.UpdateDonsPerso(details.Dons, nomPerso);
+                        }
+                        else
+                        {
+                            if (!details.Elements.IsAnyNullOrEmpty())
+                            {
+                                await DtmRepositoryUpdate.UpdateElementPerso(details.Elements, nomPerso);
+                            }
+                            else
+                            {
+                                if (!details.Inventaire.IsAnyNullOrEmpty())
+                                {
+                                    await DtmRepositoryUpdate.UpdateInventairePerso(details.Inventaire, nomPerso);
+                                }
+                                else
+                                {
+                                    if (!details.Jauges.IsAnyNullOrEmpty())
+                                    {
+                                        await DtmRepositoryUpdate.UpdateJaugesPerso(details.Jauges, nomPerso);
+                                    }
+                                    else
+                                    {
+                                        if (!details.Skills.IsAnyNullOrEmpty())
+                                        {
+                                            await DtmRepositoryUpdate.UpdateSkillsPerso(details.Skills, nomPerso);
+                                        }
+                                        else
+                                        {
+                                            if (!details.Stats.IsAnyNullOrEmpty())
+                                            {
+                                                await DtmRepositoryUpdate.UpdateStatsPerso(details.Stats, nomPerso);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-
-            if (details.Caracs.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateCaracsPerso(details.Caracs, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateJauges(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Jauges.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateJaugesPerso(details.Jauges, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateStats(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Stats.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateStatsPerso(details.Stats, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateElements(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Elements.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateElementPerso(details.Elements, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateSkills(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Skills.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateSkillsPerso(details.Skills, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateDonsPerso(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Dons.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateDonsPerso(details.Dons, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateDemons(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Demons.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateDemonsPerso(details.Demons, details.Charac.Nom);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdateInventaire(CharacterDetailsViewModel details)
-        {
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
-
-            if (details == null)
-            {
-                return NotFound();
-            }
-
-            if (details.Inventaire.IsAnyNullOrEmpty() || string.IsNullOrWhiteSpace(details.Charac.Nom))
-            {
-                return NotFound();
-            }
-
-            await DtmRepositoryUpdate.UpdateInventairePerso(details.Inventaire, details.Charac.Nom);
 
             return Ok();
         }
