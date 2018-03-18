3e8a0feb47c4885be922b0f55b8d7083df945df4
soulshyv <fra62@sfr.fr>
2018-03-19 00:21:48 +0100

    J'en peux plus

--- Main/Controllers/CharactersController.cs
+++ Main/Controllers/CharactersController.cs
@@ -7,19 +7,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DemonTaleManager.Web.Views.Characters;
 using DTM.Core.Models;
 
 namespace DemonTaleManager.Web.Controllers
 {
-    public class CharactersController : Controller/* : DtmControllerBase*/
+    public class CharactersController : DtmControllerBase
     {
-        public CharactersController(ILifetimeScope scope, CaracRepository carac)/* : base(scope)*/
+        public CharactersController(ILifetimeScope scope) : base(scope)
         {
-            Scope = scope;
-            CaracRepository = carac;
         }
 
         private ICharacPicSearcher _characPicSearcher;
@@ -31,79 +31,17 @@ namespace DemonTaleManager.Web.Controllers
         private IHostingEnvironment _hostingEnv;
         protected IHostingEnvironment HostingEnv => _hostingEnv ?? (_hostingEnv = Scope.Resolve<IHostingEnvironment>());
 
-        public ILifetimeScope Scope { get; }
-
-        //private CaracRepository _caracRepository;
-        //public CaracRepository CaracRepository => _caracRepository ?? (_caracRepository = Scope.Resolve<CaracRepository>());
-
-        private CaracRepository CaracRepository { get; }
-
-        private DemonRepository _demonRepository;
-        public DemonRepository DemonRepository => _demonRepository ?? (_demonRepository = Scope.Resolve<DemonRepository>());
-
-        private DemonPersoRepository _demonPersoRepository;
-        public DemonPersoRepository DemonPersoRepository => _demonPersoRepository ?? (_demonPersoRepository = Scope.Resolve<DemonPersoRepository>());
-
-        private DonRepository _donRepository;
-        public DonRepository DonRepository => _donRepository ?? (_donRepository = Scope.Resolve<DonRepository>());
-
-        private DonPersoRepository _donPersoRepository;
-        public DonPersoRepository DonPersoRepository => _donPersoRepository ?? (_donPersoRepository = Scope.Resolve<DonPersoRepository>());
-
-        private ElementRepository _elementRepository;
-        public ElementRepository ElementRepository => _elementRepository ?? (_elementRepository = Scope.Resolve<ElementRepository>());
-
-        private ElementPersoRepository _elementPersoRepository;
-        public ElementPersoRepository ElementPersoRepository => _elementPersoRepository ?? (_elementPersoRepository = Scope.Resolve<ElementPersoRepository>());
-
-        private InventaireRepository _inventaireRepository;
-        public InventaireRepository InventaireRepository => _inventaireRepository ?? (_inventaireRepository = Scope.Resolve<InventaireRepository>());
-
-        private ItemRepository _itemRepository;
-        public ItemRepository ItemRepository => _itemRepository ?? (_itemRepository = Scope.Resolve<ItemRepository>());
-
-        private JaugeRepository _jaugeRepository;
-        public JaugeRepository JaugeRepository => _jaugeRepository ?? (_jaugeRepository = Scope.Resolve<JaugeRepository>());
-
-        private MetierRepository _metierRepository;
-        public MetierRepository MetierRepository => _metierRepository ?? (_metierRepository = Scope.Resolve<MetierRepository>());
-
-        private MetierPersoRepository _metierPersoRepository;
-        public MetierPersoRepository MetierPersoRepository => _metierPersoRepository ?? (_metierPersoRepository = Scope.Resolve<MetierPersoRepository>());
-
-        private PassifRepository _passifcRepository;
-        public PassifRepository PassifRepository => _passifcRepository ?? (_passifcRepository = Scope.Resolve<PassifRepository>());
-
-        private PassifPersoRepository _passifPersoRepository;
-        public PassifPersoRepository PassifPersoRepository => _passifPersoRepository ?? (_passifPersoRepository = Scope.Resolve<PassifPersoRepository>());
-
-        private PassifDemonRepository _passifDemonRepository;
-        public PassifDemonRepository PassifDemonRepository => _passifDemonRepository ?? (_passifDemonRepository = Scope.Resolve<PassifDemonRepository>());
-
-        private PersoRepository _persoRepository;
-        public PersoRepository PersoRepository => _persoRepository ?? (_persoRepository = Scope.Resolve<PersoRepository>());
-
-        private SkillRepository _skillRepository;
-        public SkillRepository SkillRepository => _skillRepository ?? (_skillRepository = Scope.Resolve<SkillRepository>());
-
-        private SkillPersoRepository _skillPersoRepository;
-        public SkillPersoRepository SkillPersoRepository => _skillPersoRepository ?? (_skillPersoRepository = Scope.Resolve<SkillPersoRepository>());
-
-        private StatRepository _statRepository;
-        public StatRepository StatRepository => _statRepository ?? (_statRepository = Scope.Resolve<StatRepository>());
-
-        private UserRepository _userRepository;
-        public UserRepository UserRepository => _userRepository ?? (_userRepository = Scope.Resolve<UserRepository>());
-
+        private IEnumerable<Perso> Persos { get; set; }
+            
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var allPersos = await /*DtmRepositories.*/PersoRepository.GetAll();
+            Persos = await DtmRepositories.PersoRepository.GetAll();
 
-            if (allPersos != null)
+            if (Persos != null)
                 return View(new CharactersViewModel
                 {
-                    Persos = allPersos
+                    Persos = Persos
                 });
 
             return View();
@@ -112,17 +50,7 @@ namespace DemonTaleManager.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> GetDetails(int idPerso)
         {
-            Perso perso;
-            try
-            {
-                //var perso = await DtmRepositories.PersoRepository.GetFullPersoById(idPerso);
-                perso = await DtmRepositories.PersoRepository.GetFullPersoById(idPerso);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            var perso = await DtmRepositories.PersoRepository.GetFullPersoById(idPerso);
             var pic = GetPicture(perso.Nom) + "?" + new DateTime().TimeOfDay.Ticks;
 
             return PartialView("Details", new CharacterDetailsViewModel
@@ -163,74 +91,95 @@ namespace DemonTaleManager.Web.Controllers
             if (details == null)
                 return NotFound();
 
-            await PersoRepository.Update(details.Perso);
+            var idPerso = details.Perso.Id;
+            var persoToUpdate = Persos?.Where(_ => _.Id == idPerso).FirstOrDefault();
+            if (persoToUpdate == null)
+            {
+                return NotFound();
+            }
 
             if (!details.Caracs.IsAnyNullOrEmpty())
             {
-                await DtmRepositories.CaracRepository.Update(details.Caracs);
+                //await DtmRepositories.CaracRepository.Update(details.Caracs);
+                details.Caracs.Perso = persoToUpdate;
+                details.Caracs.PersoId = idPerso;
+                persoToUpdate.Carac = new[] {details.Caracs};
             }
             else
             {
                 if (!details.Perso.IsAnyNullOrEmpty())
                 {
-                    await DtmRepositories.PersoRepository.Update(details.Perso);
+                    //await DtmRepositories.PersoRepository.Update(details.Perso);
+                    persoToUpdate.Lvl = details.Perso.Lvl;
+                    persoToUpdate.Nom = details.Perso.Nom;
+                    persoToUpdate.Po = details.Perso.Po;
+                    persoToUpdate.Race = details.Perso.Race;
+                    persoToUpdate.TypePerso = details.Perso.TypePerso;
+                    persoToUpdate.Xp = details.Perso.Xp;
                 }
                 else
                 {
                     if (!details.DemonsPerso.IsAnyNullOrEmpty())
                     {
-                        foreach (var dp in details.DemonsPerso)
-                        {
-                            await DtmRepositories.DemonPersoRepository.Update(dp);
-                        }
+                        //foreach (var dp in details.DemonsPerso)
+                        //{
+                        //    await DtmRepositories.DemonPersoRepository.Update(dp);
+                        //}
+                        persoToUpdate.DemonPerso = details.DemonsPerso;
                     }
                     else
                     {
                         if (!details.DonsPerso.IsAnyNullOrEmpty())
                         {
-                            foreach (var dp in details.DonsPerso)
-                            {
-                                await DtmRepositories.DonPersoRepository.Update(dp);
-                            }
+                            //foreach (var dp in details.DonsPerso)
+                            //{
+                            //    await DtmRepositories.DonPersoRepository.Update(dp);
+                            //}
+                            persoToUpdate.DonPerso = details.DonsPerso;
                         }
                         else
                         {
                             if (!details.Elements.IsAnyNullOrEmpty())
                             {
-                                foreach (var e in details.Elements)
-                                {
-                                    await DtmRepositories.ElementPersoRepository.Update(e);
-                                }
+                                //foreach (var e in details.Elements)
+                                //{
+                                //    await DtmRepositories.ElementPersoRepository.Update(e);
+                                //}
+                                persoToUpdate.ElementPerso = details.Elements;
                             }
                             else
                             {
                                 if (!details.Inventaire.IsAnyNullOrEmpty())
                                 {
-                                    foreach (var inventaire in details.Inventaire)
-                                    {
-                                        await DtmRepositories.InventaireRepository.Update(inventaire);
-                                    }
+                                    //foreach (var inventaire in details.Inventaire)
+                                    //{
+                                    //    await DtmRepositories.InventaireRepository.Update(inventaire);
+                                    //}
+                                    persoToUpdate.Inventaire = details.Inventaire;
                                 }
                                 else
                                 {
                                     if (!details.Jauges.IsAnyNullOrEmpty())
                                     {
-                                        await DtmRepositories.JaugeRepository.Update(details.Jauges);
+                                        //await DtmRepositories.JaugeRepository.Update(details.Jauges);
+                                        persoToUpdate.Jauge = new[] {details.Jauges};
                                     }
                                     else
                                     {
                                         if (!details.Skills.IsAnyNullOrEmpty())
                                         {
-                                            foreach (var sp in details.Skills)
-                                            {
-                                                await DtmRepositories.SkillPersoRepository.Update(sp);
-                                            }
+                                            //foreach (var sp in details.Skills)
+                                            //{
+                                            //    await DtmRepositories.SkillPersoRepository.Update(sp);
+                                            //}
+                                            persoToUpdate.SkillPerso = details.Skills;
                                         }
                                         else
                                         {
                                             if (!details.Stats.IsAnyNullOrEmpty())
                                             {
-                                                await DtmRepositories.StatRepository.Update(details.Stats);
+                                                //await DtmRepositories.StatRepository.Update(details.Stats);
+                                                persoToUpdate.Stat = new[] {details.Stats};
                                             }
                                         }
                                     }
@@ -240,6 +189,8 @@ namespace DemonTaleManager.Web.Controllers
                     }
                 }
             }
+
+            await PersoRepository.Update(persoToUpdate);
 
             return Ok();
         }
