2513ae93607d41917db1ac44ce025c262d61cb0b
soulshyv <fra62@sfr.fr>
2018-07-17 20:27:48 +0200

    Nouveau projet RpgManager.Ged

---
+++ RpgManager.Ged/Models/GedDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RpgManager.Ged.Models
+{
+    public class GedDocument
+    {
+        public int Id { get; set; }
+        public Guid? PublicId { get; set; }
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+        public string FilePath { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
